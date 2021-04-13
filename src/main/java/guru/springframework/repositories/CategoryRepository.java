56473e2629c0656772f722d379ce39a6ecc0dfa3
dtrvno <dimatru@gmail.com>
2021-04-13 13:23:51 -0700

    added query method

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -4,6 +4,8 @@ import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.sql.rowset.CachedRowSet;
+import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository <Category,Long> {
+    Optional <Category>findByDescription(String description);
 }
