552e8cc545d43ac3432e621be936f5424c09505b
dtrvno <dimatru@gmail.com>
2021-04-13 12:42:03 -0700

    loaded repo, fixed typo

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -11,6 +11,7 @@ public class UnitOfMeasure
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String description;
 
     public Long getId() {
         return id;
@@ -20,13 +21,11 @@ public class UnitOfMeasure
         this.id = id;
     }
 
-    public String getDesctiption() {
-        return desctiption;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDesctiption(String desctiption) {
-        this.desctiption = desctiption;
+    public void setDescription(String description) {
+        this.description = description;
     }
-
-    private String desctiption;
 }
